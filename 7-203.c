982d1f04f716f1b8a5bf562bd1b252f06ee1697b
warpatte <3248471953@qq.com>
2023-04-07 22:00:44 +0800

    Revert "0"
    This reverts commit 48535ac3bf58d74b80c026fdc1ece34e54f5503d.

--- 7-203.c
+++ 7-203.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int a, b, n;
-    double j;
+    int a, b, n, j = 1;
     scanf("%d %d",&a, &b);
     n = a+b;
-    j = fact(n);
-    printf("%f",j);
+    for(int i = 1; i<=n; i++){
+        j *= i;
+    }
+    printf("%d",j);
     return 0;
 }
